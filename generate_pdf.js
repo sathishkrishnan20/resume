96213aba8281127266fe16ec4a0560c2966e347c
Sathish Krishnan <Sathishkrish20@gmail.com>
2021-12-27 22:09:56 +0530

    pdf-resume-generated

---
+++ generate_pdf.js
@@ -0,0 +1,14 @@
+var html_to_pdf = require('html-pdf-node');
+let options = { format: 'A4', path: 'resume.pdf', scale: 0.7, printBackground: true, margin: {
+    top: 5,
+    bottom: 5,
+    left: 8,
+    right: 8
+} };
+// Example of options with args //
+// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
+
+let file = { url: "http://192.168.1.19:8887" };
+
+html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+});
