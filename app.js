e157db29d3429328f040f8b10cf21f5a3e82d974
Sathish Krishnan <Sathishkrish20@gmail.com>
2021-12-27 11:43:08 +0530

    Resume integrated with Node app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.static('public'))
+
+// sendFile will go here
+var a = ['/', '/blabla', '/blablablabla'];
+// app.get(a, express.static(path.join(__dirname, 'public')))
+app.route('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
