5fe8656b92349b0fd43745f754d7d12f117ae988
Sathish Krishnan <Sathishkrish20@gmail.com>
2021-12-28 11:42:44 +0530

    API added for Read Resume

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
-
+const { readResumeContents } = require('./gsheet')
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.static('public'))
 
 app.route('/', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/read_resume', async function(req, res) {
+    await readResumeContents(req, res);
 });
 
 app.listen(port);
