df7624ebcfaac2983a86b59d7677d997dc0209c1
Sathish Krishnan <Sathishkrish20@gmail.com>
2022-01-08 09:29:26 +0530

    New Work Exp Added

--- generate_pdf.js
+++ generate_pdf.js
@@ -1,7 +1,7 @@
 var html_to_pdf = require('html-pdf-node');
-let options = { format: 'A4', path: 'resume.pdf', scale: 0.7, printBackground: true, margin: {
-    top: 5,
-    bottom: 5,
+let options = { format: 'A4', path: 'resume.pdf', scale: 0.70, printBackground: true, margin: {
+    top: 4,
+    bottom: 8,
     left: 8,
     right: 8
 } };
@@ -11,4 +11,5 @@ let options = { format: 'A4', path: 'resume.pdf', scale: 0.7, printBackground: t
 let file = { url: "http://192.168.1.19:8887" };
 
 html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+
 });
